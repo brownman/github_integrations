2204d1ac101d022942dec3da694580bac5c13f13
brownman <brownman556@gmail.com>
2015-01-01 20:02:39 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
-export MODE=${1:-remote}
 
 dir_self=$( cd `dirname $0`; pwd )
 chmod u+x *.sh . -R
 
 source $dir_self/cfg/config.cfg
 
-if [ "$MODE" = local ];then
-       cat $dir_self/sh/install.sh
-       ls -l $dir_self/local/*.sh
-else
-      bash -c $dir_self/sh/install.sh
-      source $dir_self/cfg/override.cfg
-      $dir_self/sh/ensure.sh || { 
-      toilet --metal "OOPS"
-      echo 'YOU NEED TO UPDATE .travis.yml;';
-      echo 'HELP YOURSELF BY VISITING:';
-      echo 'https://github.com/brownman/github_integrations';
-      exit 1;
-    }
-    bash -c $dir_self/remote/update-gh-pages.sh
-fi
+
+bash -c $dir_self/sh/install.sh
+source $dir_self/cfg/override.cfg
+$dir_self/sh/ensure.sh || { 
+toilet --metal "OOPS"
+echo 'YOU NEED TO UPDATE .travis.yml;';
+echo 'HELP YOURSELF BY VISITING:';
+echo 'https://github.com/brownman/github_integrations';
+exit 1;
+}
+
+bash -c $dir_self/remote/update-gh-pages.sh
+
