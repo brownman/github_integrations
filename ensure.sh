5f6492c7f11af3553b7ce3aa21e5db88519a50f3
brownman <brownman556@gmail.com>
2015-01-01 18:04:21 +0200

    Update ensure.sh

--- ensure.sh
+++ ensure.sh
@@ -1,1 +1,2 @@
-test -v GH_TOKEN || { echo 'https://github.com/brownman/github_integrations/blob/master/BANK/travis_and_github/README.md';echo; cat BANK/travis_and_github/README.md; }
+#!/usr/bin/env bash
+test -v GH_TOKEN
