5c9871d0530d283dfa528e6ac589670a2fff11db
brownman <brownman556@gmail.com>
2015-01-01 18:55:31 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@
 dir_self=$( cd `dirname $0`; pwd )
 chmod u+x *.sh . -R
 
-$dir_self/BANK/travis_and_github/install.sh
+bash -c $dir_self/install.sh
 source $dir_self/config.cfg
 source $dir_self/override.cfg
 
