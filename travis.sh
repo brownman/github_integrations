176bfc04870cfef6878bd188e03d4368d40ca877
brownman <brownman556@gmail.com>
2015-01-01 19:17:04 +0200

    [MODE] local VS remote

--- travis.sh
+++ travis.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
+export MODE=${remote:-local}
 
 dir_self=$( cd `dirname $0`; pwd )
 chmod u+x *.sh . -R
 
-bash -c $dir_self/install.sh
-source $dir_self/config.cfg
-source $dir_self/override.cfg
+source $dir_self/cfg/config.cfg
 
- 
-$dir_self/ensure.sh || { 
- toilet --metal "OOPS"
-  echo 'YOU NEED TO UPDATE .travis.yml;';
-  echo 'HELP YOURSELF BY VISITING:';
-  echo 'https://github.com/brownman/github_integrations';
-  exit 1;
-}
-
-bash -c $dir_self/travis/update-gh-pages.sh; 
+if [ "$MODE" = local ];then
+       ls -l $dir_self/local/*.sh
+else
+    bash -c $dir_self/sh/install.sh
+    source $dir_self/cfg/override.cfg
+    $dir_self/sh/ensure.sh || { 
+     toilet --metal "OOPS"
+      echo 'YOU NEED TO UPDATE .travis.yml;';
+      echo 'HELP YOURSELF BY VISITING:';
+      echo 'https://github.com/brownman/github_integrations';
+      exit 1;
+    }
+    bash -c $dir_self/remote/update-gh-pages.sh
+fi
