6e51948ebb5593e291c87ed092248abaee97c814
brownman <brownman556@gmail.com>
2015-01-01 18:49:01 +0200

    Update install.sh

--- BANK/travis_and_github/install.sh
+++ BANK/travis_and_github/install.sh
@@ -1,3 +1,10 @@
-gem install travis
-sudo apt-get install xsel
+#!/usr/bin/env bash
+
+if [ "$LOGNAME" = travis ];then
+  sudo apt-get install figlet toilet
+else
+  gem install travis
+  sudo apt-get install xsel
+fi
+
 
