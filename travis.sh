dd95cf0fbf86b1ec8ade8422c89b8a56995fd37c
brownman <brownman556@gmail.com>
2015-01-01 18:49:59 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -3,12 +3,13 @@
 dir_self=$( cd `dirname $0`; pwd )
 chmod u+x *.sh . -R
 
-
+$dir_self/BANK/travis_and_github/install.sh
 source $dir_self/config.cfg
 source $dir_self/override.cfg
 
  
 $dir_self/ensure.sh && { bash -c $dir_self/BANK/travis_and_github/update-gh-pages.sh; } || {
+ toilet --metal "OOPS"
   echo 'YOU NEED TO UPDATE .travis.yml;';
   echo 'HELP YOURSELF BY VISITING:';
   echo 'https://github.com/brownman/github_integrations';
