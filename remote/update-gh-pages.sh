41a18e58439964168976a97c9ccff68c497fc8d1
brownman <brownman556@gmail.com>
2015-01-02 20:46:59 +0200

    Update update-gh-pages.sh

--- remote/update-gh-pages.sh
+++ remote/update-gh-pages.sh
@@ -31,6 +31,14 @@ setup_git_local(){
   echo "https://${GH_TOKEN}:@github.com" > .git/credentials
   debug_git
 }
+git_create_branch(){
+  #http://www.zorched.net/2008/04/14/start-a-new-branch-on-your-remote-git-repository/
+local  branch_name = $1
+git push origin origin:refs/heads/${branch_name}
+git fetch origin
+git checkout --track -b ${branch_name} origin/${branch_name}
+git pull
+}
 
 clone1(){
   
@@ -38,18 +46,19 @@ clone1(){
   echo "Starting to update $branch"
 #  commander cd $HOME
   git checkout -B gh_pages
-  $( git branch -r | grep $branch )
+  ( git branch -r | grep $branch )
   res=$?
   echo we have remote branch named gh-pages? $res
   
-  if [ $res -eq 0 ];then
+  if [ $res -eq 1 ];then
+        git_create_branch $branch
+ fi
+ #   commander dir_gh_pages=$PWD
+    #trace git checkout
+    #commander git checkout -B $branch
     trace git clone
     git clone --depth=1 --quiet --branch=$branch https://${GH_TOKEN}@github.com/$owner/$repo.git $dir_gh_pages > /dev/null 
- else
-    commander dir_gh_pages=$PWD
-    trace git checkout
-    commander git checkout -B $branch
-  fi
+
   
   #setup_git_local
 }
