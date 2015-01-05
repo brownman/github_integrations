660c293839c3eb1abdda03af22aa8399a98f4b7f
brownman <brownman556@gmail.com>
2015-01-05 14:19:58 +0200

    Update install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$LOGNAME" = travis ];then
-  sudo apt-get install figlet toilet
+  sudo apt-get install figlet toilet tree
 else
   gem install travis
   sudo apt-get install xsel
